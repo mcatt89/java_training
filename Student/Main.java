6157048c7f8ff60fa53aefcc99171772a4f98e0b
mcatt89 <mcatt89@gmail.com>
2024-12-18 22:34:29 -0600

    initial work

---
+++ Student/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        //Student data
+        String studentName = "John Doe";
+        int studentID = 15;
+        int studentAge = 23;
+        float studentFee = 15.25f;
+        char studentGrade = 'B';
+
+        // Print variables
+        System.out.println("Student Name: " + studentName);
+        System.out.println("Student ID: " + studentID);
+        System.out.println("Student Age: " + studentAge);
+        System.out.println("Student Fee: " + studentFee);
+        System.out.println("Student Grade: " + studentGrade);
+    }
+}
---
+++ zhello_world/Main.java
@@ -0,0 +1,13 @@
+//Output text
+public class Main { //end line comment
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("I am learning Java");
+        System.out.println(123);
+    }
+}
+
+/* 
+ * Multiline
+ * Comment
+ */
